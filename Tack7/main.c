91699df044fb4e730cad3e7bfd82e670484e91b6
Neznaika76 <n9032692530@gmail.com>
2024-01-08 12:33:52 +0300

    Задача 2

--- Tack7/main.c
+++ Tack7/main.c
@@ -4,12 +4,14 @@
 //
 //  Created by Pavel iPro on 08.01.2024.
 //
+/*Задача 1
 
 #include <stdio.h>
 
 const int amount = 5;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) 
+{
 int m[amount];
     for (int i = 0; i < amount; i++)
         scanf("%d", &m[i]);
@@ -19,4 +21,21 @@ int m[amount];
     printf("%.3f\n", (float)sum / amount);
     return 0;
 }
+*/
+/* Задача 2 */
 
+#include <stdio.h>
+
+const int amount = 5;
+
+int main(int argc, const char * argv[])
+{
+int m[amount];
+    for (int i = 0; i < amount; i++)
+        scanf("%d", &m[i]);
+int min = m[0];
+    for (int i = 1; i < amount; i++)
+        if (m[i] < min)
+            min = m[i];
+    printf("%d\n", min);
+return 0; }
